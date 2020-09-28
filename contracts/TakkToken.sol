c7d92b93ae9742a35f6eade218187af97db9db1f
Jess <jtrinh619@gmail.com>
2020-09-28 11:42:11 -0700

    implement token creation and mapping - tokens are storable in console

--- contracts/TakkToken.sol
+++ contracts/TakkToken.sol
@@ -8,6 +8,28 @@ contract TakkToken {
     // state variable to keep track of number of gratitude 
     uint public gratitudeCount = 0;
 
+    // Gratitude tokens struct 
+    struct Gratitude {
+        uint id; // id of tokens
+        string gratitudeMessage; // message message of gratitude attached to token -- optional
+
+    }
+
+    // store tokens
+    mapping(uint => Gratitude) public tokens;
+
+    // populate token to store
+    constructor() public {
+        createToken("Welcome Takk Token"); // default token when user first joins and opens wallet
+    }
+
+    // creates token with id & message 
+    function createToken(string memory _content) public {
+        gratitudeCount ++; 
+        // put token inside mapping 
+        tokens[gratitudeCount] = Gratitude(gratitudeCount, _content);
+    }
+
 }
 
 
