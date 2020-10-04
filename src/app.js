ccf9bc77de28e42cf95d8828261f4a1cea2dc449
kmurata798 <kmurata798@gmail.com>
2020-10-04 16:00:04 -0700

    starting on loadContract function

--- src/app.js
+++ src/app.js
@@ -9,45 +9,49 @@ App = {
       // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
     loadWeb3: async () => {
         if (typeof web3 !== 'undefined') {
-          App.web3Provider = web3.currentProvider
-          web3 = new Web3(web3.currentProvider)
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
         } else {
-          window.alert("Please connect to Metamask.")
+            window.alert("Please connect to Metamask.")
         }
         // Modern dapp browsers...
         if (window.ethereum) {
-          window.web3 = new Web3(ethereum)
-          try {
-            // Request account access if needed
-            await ethereum.enable()
-            // Acccounts now exposed
-            web3.eth.sendTransaction({/* ... */})
-          } catch (error) {
-            // User denied account access...
-          }
+            window.web3 = new Web3(ethereum)
+            try {
+                // Request account access if needed
+                await ethereum.enable()
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */})
+            } catch (error) {
+                // User denied account access...
+            }
         }
         // Legacy dapp browsers...
         else if (window.web3) {
-          App.web3Provider = web3.currentProvider
-          window.web3 = new Web3(web3.currentProvider)
-          // Acccounts always exposed
-          web3.eth.sendTransaction({/* ... */})
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */})
         }
         // Non-dapp browsers...
         else {
-          console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
         }
       },
     loadAccount: async () => {
         // Set the current blockchain account
         App.accountME = web3.eth.accounts[0]
+        // Check imported Metamask ganache account id
         console.log(App.accountME)
-      },
+    },
 }
+
+    // Kento: Load smart contract from the blockchain
+    loadContract:
 
 $(() => {
     $(window).load(() => {
-      App.load()
+        App.load()
     })
-  })
+})
   
