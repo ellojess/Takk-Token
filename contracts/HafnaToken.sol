aec9d6913bf831cbc00952e6829cffda21cd3f85
Jess <jtrinh619@gmail.com>
2020-10-09 23:25:50 -0700

    develop functionality for users to create hafna token

--- contracts/HafnaToken.sol
+++ contracts/HafnaToken.sol
@@ -1,10 +1,33 @@
 pragma solidity ^0.6.0; 
 
-contract HafnaToken {
+import "./Ownable.sol";
+import "./safemath.sol";
+
+contract HafnaToken is Ownable {
+
+    using SafeMath for uint256;
+
+    event NewHafna(uint id, string name);
+
     // variables 
     uint public gratitudeCount = 0; // track takk existing 
-
     bool hafnaDoesExist = false; // see if hafna token exists in wallet 
+
+    struct Hafna {
+        uint id; // id of token 
+        bool exists; // see if it exists at any moment 
+        string name; // name Hafna token 
+    }
+
+    mapping(uint => Hafna) public hafna; // store hafna in blockchain/db 
+
+    function createHafna(string memory uint _id, bool _exists, string _name) public {
+        // this function creates a Hafna token
+        uint id = Hafna.push(Hafna(_id, _exists, _name));
+        emit NewHafna(id, _name);
+    }
+
+
 
 
 
