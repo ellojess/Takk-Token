d1c1d9164595202ca31d97df2bfc853593f92665
Jess <jtrinh619@gmail.com>
2020-10-09 23:30:07 -0700

    [review] develop transfer token functionality

--- contracts/HafnaToken.sol
+++ contracts/HafnaToken.sol
@@ -21,10 +21,15 @@ contract HafnaToken is Ownable {
 
     mapping(uint => Hafna) public hafna; // store hafna in blockchain/db 
 
-    function createHafna(string memory uint _id, bool _exists, string _name) public {
-        // this function creates a Hafna token
+    // create a Hafna token
+    function createHafna(string memory _content, address _to, uint _id, bool _exists, string _name) public {
         uint id = Hafna.push(Hafna(_id, _exists, _name));
         emit NewHafna(id, _name);
+    }
+
+    // transfer token to another user
+    function transfer(address from, address to, uint256 tokenId) public {
+        safeTransferFrom(from, to, tokenId);
     }
 
 
