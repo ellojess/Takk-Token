b93a4cc4dd022bc945ba5ed2fde6dab8edb216b1
kmurata798 <kmurata798@gmail.com>
2020-10-09 20:14:55 -0700

    Help from Jaeson

--- contracts/TakkToken.sol
+++ contracts/TakkToken.sol
@@ -20,24 +20,71 @@ contract TakkToken is ERC721, Ownable {
     // store tokens
     mapping(uint => Gratitude) public tokens;
 
+    mapping(address => uint[]) public owners;
+
     // populate token to store
     // constructor() public {
     //     createToken("Welcome Takk Token"); // default token when user first joins and opens wallet
     // }
 
     // this contructor is needed for the ERC721 inheritance to work
-    constructor() ERC721("GameItem", "ITM") public { 
+    constructor() ERC721("TakkToken", "TT") public { 
         // createToken("Welcome Takk Token");
+    }
+    
+    // Function to show current number of gratitude a user has**
+
+    // similar to show tokens, just get their address (msg.sender), check if anything is in their wallet => wallet is their address
+    // create mapping holding a list of tokens they own, add to the mapping in the create token function 
+    // (Key is the person's address, value is array of tokens they have) 
+    // may have to create a temporary memory of the tokens they hold, create new array, make that array the new value for the address (MAY HAVE TO DO)
+    // 
+
+    // // Show the owner's gratitude tokens
+    // function showTokens(uint tokenId) public view returns address {
+    //     // pass in token id => return address of the owner
+    //     require (msg.sender == ownerOf(tokenId))
+    //     return ownerOf(tokenId)
+    // }
+
+    // Show the owner's gratitude tokens
+    function showTokens() public view returns (uint[] memory) {
+        // pass in token id => return address of the owner
+        // might be .length
+        require (owners[msg.sender].length != 0);
+        return owners[msg.sender];
+    }
+
+    function transfer(address from, address to, uint256 tokenId) {
+        safeTransferFrom(from, to, tokenId);
     }
 
     // creates token with id & message 
-    function createToken(address _to, string memory _content) public  {
+    function createToken(address _to, string memory _content) public {
         gratitudeCount ++; 
+
         // put token inside mapping 
         tokens[gratitudeCount] = Gratitude(gratitudeCount, _content);
-
+        // run showTokens in createToken, then add to that array
+        // if it doesn't create anything, create new array
+        // return show token, set it, then add it
+        uint[] memory ownersTokens = showTokens(_to);
+        if (ownersTokens.length != 0) {
+            ownersTokens.push(gratitudeCount);
+            // updating the mapping
+            owners[_to] = ownersTokens;
+        }
+        else {
+            // creating new mapping
+            owners[_to] = uint[gratitudeCount];
+        }
         super._mint(_to, gratitudeCount);
+
     }
+
+    // extra function to transfer an existing token to another user.
+    // IF we deploy, Rinkeby (least buggy), Kovan
+
 
 }
 
